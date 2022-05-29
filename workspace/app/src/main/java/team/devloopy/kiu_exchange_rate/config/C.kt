d646024232585c79b38cefeb03f1787d026a31fd
kyu_chan <41310752+kyuchanE@users.noreply.github.com>
2022-05-30 06:37:05 +0900

    commit:

---
+++ workspace/app/src/main/java/team/devloopy/kiu_exchange_rate/config/C.kt
@@ -0,0 +1,10 @@
+package team.devloopy.kiu_exchange_rate.config
+
+object C {
+
+    object ApiLayer {
+        val url: String = "http://www.apilayer.net/api/live"
+        val key: String = "38C4bnbI9OysiWf0T3X6Ht83qDPvCbTE"
+    }
+
+}
